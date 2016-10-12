dc9e54c14c52cdffd2d5eb9d869ad88ca07a535b
paulweaver2011 <paulweaver2011@gmail.com>
2016-10-12 10:59:50 -0600

    initial commit from last time.

---
+++ JS/Final_Solution.js
@@ -0,0 +1,6 @@
+//Opening narration prior to character creation, naming, etc.
+alert("Pain. Everywhere, pain. Your mind swims as you grasp for control over your body. Your lungs crack as you try to breath, gasping and coughing. Best to take only shallow breaths for now. ");
+alert("The darkness is complete around you. You struggle to open your swollen eyes, and are at last able to blink the crusted sleep away. Though blurry, your vision begins to clear and you see a faint glow through dark bands");
+alert("You feel around with your bruised fingers. The surface below you feels hard and cold, and damp. You force yourself into a sitting positions and nudge your way to the banded darkness.");
+alert("You reach outward and are stopped by something rough to the touch. Iron. Bars. Of course. Iron bars, my dear old friend; it seems this day we meet again. You clumbsily pull yourself up to a standing position, straining your pulsing muscles");
+
